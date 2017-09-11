616678ea620dad08ec2774bf85b635fc6206fc7f
sellernvcu <sellern@vcu.edu>
2017-09-10 21:24:59 -0400

    With Loop

--- ChangeCounter/ChangeCounter.cpp
+++ ChangeCounter/ChangeCounter.cpp
@@ -15,43 +15,49 @@ int main()
 	int QuarterChange;
 	int DimeChange;
 	int NickelChange;
+	while (true)
+	{
+		cout << "Welcome to the change machine of magic. Please enter the cost of your item. (Remember our machine does not take penny values! Or Enter 0 to exit.)" << endl;
+		cin >> ItemCost;
 
-	cout << "Welcome to the change machine of magic. Please enter the cost of your item. (Remember our machine does not take penny values!)" << endl;
-	cin >> ItemCost;
-	cout << "Please enter the amount you wish to pay." << endl;
-	cin >> PaymentAmount;
+		if (ItemCost == 0)
+			break;
 
-while (PaymentAmount< ItemCost)
-	{ cout << "Insufficient Funds." << endl;
-	cout << "Please add funds. Enter the amount you wish to add." << endl;
-	cin >> AddFunds;
-	PaymentAmount = PaymentAmount + AddFunds;
+		cout << "Please enter the amount you wish to pay." << endl;
+		cin >> PaymentAmount;
+
+		while (PaymentAmount < ItemCost)
+		{
+			cout << "Insufficient Funds." << endl;
+			cout << "Please add funds. Enter the amount you wish to add." << endl;
+			cin >> AddFunds;
+			PaymentAmount = PaymentAmount + AddFunds;
+		}
+
+		cout << "You are paying " << PaymentAmount << endl;
+
+		if (PaymentAmount == ItemCost)
+			cout << "No Change Due" << endl;
+
+		else if (PaymentAmount > ItemCost)
+		{
+			ChangeDue = PaymentAmount - ItemCost;
+			cout << "Your change is " << ChangeDue << endl;
+			DollarChange = ChangeDue / 1.00;
+			int RemainChangeDue = (ChangeDue - DollarChange) * 100;
+			QuarterChange = RemainChangeDue / 25;
+			DimeChange = (RemainChangeDue %= 25) / 10;
+			NickelChange = (RemainChangeDue %= 10) / 5;
+
+			cout << "That is equal to " << endl;
+			cout << DollarChange << " Dollars" << endl;
+			cout << QuarterChange << " Quarters" << endl;
+			cout << DimeChange << " Dimes" << endl;
+			cout << NickelChange << " Nickels" << endl;
+
+		}
+
 	}
-
-cout << "You are paying " << PaymentAmount << endl;
-
-if (PaymentAmount == ItemCost)
-cout << "No Change Due" << endl;
-
-else if (PaymentAmount > ItemCost)
-{
-	    ChangeDue = PaymentAmount - ItemCost;
-		cout << "Your change is "<< ChangeDue << endl;
-		DollarChange = ChangeDue / 1.00;
-		int RemainChangeDue = (ChangeDue - DollarChange) * 100;
-		QuarterChange = RemainChangeDue / 25;
-		DimeChange = (RemainChangeDue%=25) / 10;
-		NickelChange = (RemainChangeDue%=10) / 5;
-
-		cout << "That is equal to " << endl; 
-		cout << DollarChange << " Dollars" << endl;
-		cout << QuarterChange << " Quarters" << endl;
-		cout << DimeChange << " Dimes" << endl;
-		cout << NickelChange << " Nickels" << endl;
-
-}
-
-
 
    return 0;
 }
